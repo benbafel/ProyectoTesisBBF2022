69bd32a0f0bbccb350421341fb5d9f5b3826eecb
benbafel <benjaminbarahonaf@gmail.com>
2022-05-15 01:32:40 -0400

    kotlin y nuevas actividades

--- app/src/main/java/com/benbafel/starprologin/AuthActivity.kt
+++ app/src/main/java/com/benbafel/starprologin/AuthActivity.kt
@@ -5,13 +5,28 @@ import android.os.Bundle
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_auth.*
 
 
 class AuthActivity : AppCompatActivity() {
+
     private lateinit var analytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
         analytics = Firebase.analytics
+
+        //Setup
+        setup()
+
+    }
+
+    private fun setup() {
+        title = "Autenticación"
+        loginButton
+
     }
 }
+
---
+++ app/src/main/java/com/benbafel/starprologin/TestHomeActivity.kt
@@ -0,0 +1,11 @@
+package com.benbafel.starprologin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_home)
+    }
+}
