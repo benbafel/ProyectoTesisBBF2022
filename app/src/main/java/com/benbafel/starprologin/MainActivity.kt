10dd0dc7ce1ec584307b44b8dacefc127f38842a
benbafel <benjaminbarahonaf@gmail.com>
2022-05-14 22:01:04 -0400

    implementacion  google analytics

--- app/src/main/java/com/benbafel/starprologin/MainActivity.kt
+++ app/src/main/java/com/benbafel/starprologin/MainActivity.kt
@@ -2,10 +2,16 @@ package com.benbafel.starprologin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var analytics: FirebaseAnalytics
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        analytics = Firebase.analytics
     }
 }
